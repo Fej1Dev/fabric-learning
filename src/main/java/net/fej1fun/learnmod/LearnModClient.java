2880dcaa10add365ef71bb221e99c942c5f20dfe
Fej1Dev <101066376+Fej1Dev@users.noreply.github.com>
2022-12-07 19:23:31 +0200

    Initial commit

---
+++ src/main/java/net/fej1fun/learnmod/LearnModClient.java
@@ -0,0 +1,10 @@
+package net.fej1fun.learnmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearnModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
